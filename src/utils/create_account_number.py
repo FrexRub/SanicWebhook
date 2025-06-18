6e9346a9bdc4d605690101a45a29f4a9494b15c9
Станислав Рубцов <frexrub@gmail.com>
2025-06-18 10:27:02 +0300

    edit create_account_number.py(add bank_account)

--- src/utils/create_account_number.py
+++ src/utils/create_account_number.py
@@ -1,6 +1,12 @@
 import random
 import asyncio
 
+from sqlalchemy import select, exists
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.payments.models import Score
+
 
 async def generate_bank_account(prefix: str = "40817", length: int = 20) -> str:
     """
@@ -15,6 +21,27 @@ async def generate_bank_account(prefix: str = "40817", length: int = 20) -> str:
         [str(random.randint(0, 9)) for _ in range(unique_part_length)]
     )
     await asyncio.sleep(0.1)
-
     account_number = prefix + unique_part
+
+    return account_number
+
+
+async def bank_account(
+    session: AsyncSession, prefix: str = "40817", length: int = 20
+) -> str:
+    """
+    Генерирует уникального номер банковского счета.
+    """
+    account_number = await generate_bank_account(prefix=prefix, length=length)
+
+    stmt = select(exists().where(Score.account_number == account_number))
+    res: Result = await session.execute(stmt)
+    is_exists: bool = res.scalar()
+
+    while is_exists:
+        account_number = await generate_bank_account(prefix=prefix, length=length)
+        stmt = select(exists().where(Score.account_number == account_number))
+        res: Result = await session.execute(stmt)
+        is_exists: bool = res.scalar()
+
     return account_number
